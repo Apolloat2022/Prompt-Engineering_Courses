ee1c5fcc7bc78d3c8beae5a33fb1d5234fbf1679
Apolloat2022 <revanaglobal@gmail.com>
2025-12-25 13:36:03 -0600

    build: remove UTF-8 BOM from config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-﻿/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -15,3 +15,4 @@ module.exports = {
   },
   plugins: [],
 }
+
